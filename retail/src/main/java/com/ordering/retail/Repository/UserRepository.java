cb9fffa795e360be39068e178154d3fa22709844
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-05-25 13:30:19 +0530

    Added Repository Files

--- retail/src/main/java/com/ordering/retail/Repository/UserRepository.java
+++ retail/src/main/java/com/ordering/retail/Repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.ordering.retail.Repository;
 
-public class UserRepository {
-    
+import com.ordering.retail.Entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
