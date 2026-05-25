f7bac31f120884a9343932ba8424dcd97c1e7f3c
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-05-25 15:34:29 +0530

    changes in Java

--- retail/src/main/java/com/ordering/retail/Enum/DiscountType.java
+++ retail/src/main/java/com/ordering/retail/Enum/DiscountType.java
@@ -1,10 +1,5 @@
 package com.ordering.retail.Enum;
 
 public enum DiscountType {
-<<<<<<< HEAD
-    FLAT,
-    PERCENTAGE
-=======
     FLAT, PERCENTAGE
->>>>>>> 5184298ba81ef829889e69f270ea5c18e9e6d1f0
 }
